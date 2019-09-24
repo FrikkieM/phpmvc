a436cac82d690837356e88fb3598ceb637a0ee23
FrikkieM <frikkieweb@gmail.com>
2019-09-24 13:38:48 +0200

    Todo: PDO Database

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+    //App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    //URL Root
+    define('URLROOT', 'http://localhost/phpmvc');
+    //Site Name
+    define('SITENAME', 'PHP MVC App');
+    
+
+
+?>
